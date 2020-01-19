5d1d87eb9431bcc080c32f1237d51906eb9717f5
huzhenyang <zhenyang.hu@happy-seed.com>
2020-01-19 17:50:41 +0800

    格式化代码

--- concurrent/src/main/java/cn/enjoyedu/ch1/wn/TestWN.java
+++ concurrent/src/main/java/cn/enjoyedu/ch1/wn/TestWN.java
@@ -1,32 +1,32 @@
 package cn.enjoyedu.ch1.wn;
 
 /**
- *类说明：测试wait/notify/notifyAll
+ * 类说明：测试wait/notify/notifyAll
  */
 public class TestWN {
-    private static Express express = new Express(0,Express.CITY);
+    private static Express express = new Express(0, Express.CITY);
 
     /*检查里程数变化的线程,不满足条件，线程一直等待*/
-    private static class CheckKm extends Thread{
+    private static class CheckKm extends Thread {
         @Override
         public void run() {
-        	express.waitKm();
+            express.waitKm();
         }
     }
 
     /*检查地点变化的线程,不满足条件，线程一直等待*/
-    private static class CheckSite extends Thread{
+    private static class CheckSite extends Thread {
         @Override
         public void run() {
-        	express.waitSite();
+            express.waitSite();
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
-        for(int i=0;i<3;i++){
+        for (int i = 0; i < 3; i++) {
             new CheckSite().start();
         }
-        for(int i=0;i<3;i++){
+        for (int i = 0; i < 3; i++) {
             new CheckKm().start();
         }
 
