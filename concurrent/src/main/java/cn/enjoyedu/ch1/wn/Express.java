91a7bf85d327911a0103fde39653430940aa9c7c
huzhenyang <zhenyang.hu@happy-seed.com>
2020-01-19 17:34:16 +0800

    格式化代码

--- concurrent/src/main/java/cn/enjoyedu/ch1/wn/Express.java
+++ concurrent/src/main/java/cn/enjoyedu/ch1/wn/Express.java
@@ -1,7 +1,7 @@
 package cn.enjoyedu.ch1.wn;
 
 /**
- *类说明：快递实体类
+ * 类说明：快递实体类
  */
 public class Express {
     public final static String CITY = "ShangHai";
@@ -17,43 +17,43 @@ public class Express {
     }
 
     /* 变化公里数，然后通知处于wait状态并需要处理公里数的线程进行业务处理*/
-    public synchronized void changeKm(){
+    public synchronized void changeKm() {
         this.km = 101;
         notify();
     }
 
     /* 变化地点，然后通知处于wait状态并需要处理地点的线程进行业务处理*/
-    public  synchronized  void changeSite(){
+    public synchronized void changeSite() {
         this.site = "BeiJing";
         notifyAll();
     }
 
     /*线程等待公里的变化*/
-    public synchronized void waitKm(){
-        while(this.km<100){
+    public synchronized void waitKm() {
+        while (this.km < 100) {
             try {
                 wait();
                 System.out.println("Check Site thread["
-                                +Thread.currentThread().getId()
-                        +"] is be notified");
+                        + Thread.currentThread().getId()
+                        + "] is be notified");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println("the Km is "+this.km+",I will change db");
+        System.out.println("the Km is " + this.km + ",I will change db");
     }
 
     /*线程等待目的地的变化*/
-    public synchronized void waitSite(){
-        while(this.site.equals(CITY)){//快递到达目的地
+    public synchronized void waitSite() {
+        while (this.site.equals(CITY)) {//快递到达目的地
             try {
                 wait();
-                System.out.println("Check Site thread["+Thread.currentThread().getId()
-                		+"] is be notified");
+                System.out.println("Check Site thread[" + Thread.currentThread().getId()
+                        + "] is be notified");
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
-        System.out.println("the site is "+this.site+",I will call user");
+        System.out.println("the site is " + this.site + ",I will call user");
     }
 }
